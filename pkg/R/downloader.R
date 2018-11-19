eed8b78b227dd01def9a84b398180701a7ab335f
Thorsten Simon <thorsten.simon@uibk.ac.at>
2018-11-19 11:58:21 +0100

    1st version of pkg

--- pkg/R/downloader.R
+++ pkg/R/downloader.R
@@ -1,10 +1,5 @@
-download_TanDEM <- function( lon = c(7, 16), lat = c(45, 49), fact = 5, usr = "", pwd = "") {
-## source("~/Projects/TamDEM-R/pkg/R/downloader.R")
- 
-    # ----------------------------------------------------------------
-    # Loading required packages.
-    # ----------------------------------------------------------------
-    require("raster")
+download_TanDEM <- function(lon = c(7, 16), lat = c(45, 49), fact = 5, usr = "", pwd = "",
+                            return.raster = TRUE) {
  
     # ----------------------------------------------------------------
     # TanDEM-X is available in tiles, 1 times 1 degree each.
@@ -21,29 +16,31 @@ download_TanDEM <- function( lon = c(7, 16), lat = c(45, 49), fact = 5, usr = ""
         url  <- sprintf("%sN%02d/E%03d/", baseurl, ll$lat[i], (ll$lon[i] %/% 10) * 10)
 
         ## cat(sprintf("   Downloading %s\n",file))
-        if ( file.exists(sprintf("~tmp/TDM90/%s_DEM.tif", file)) ) next
+        if(!file.exists(sprintf("~tmp/TDM90/%s_DEM.tif", file))) {
 
-        download.file(url      = sprintf("https://%s%s.zip", url, file),
-                      destfile = sprintf("~/tmp/TDM90/%s.zip", file),
-                      method   = "wget",
-                      extra    = sprintf("--auth-no-challenge --user=%s --password=%s", usr, pwd))
+            download.file(url = sprintf("https://%s%s.zip", url, file),
+                     destfile = sprintf("~/tmp/TDM90/%s.zip", file),
+                     method   = "wget",
+                     extra    = sprintf("--auth-no-challenge --user=%s --password=%s", usr, pwd))
 
-        system(sprintf("cd ~/tmp/TDM90; unzip %s.zip; rm %s.zip; cp %s_V01_C/DEM/%s_DEM.tif .; rm -r %s_V01_C/", file, file, file, file, file))
+            system(sprintf("cd ~/tmp/TDM90; unzip %s.zip; rm %s.zip; cp %s_V01_C/DEM/%s_DEM.tif .; rm -r %s_V01_C/", file, file, file, file, file))
+        }
     }
  
     # ----------------------------------------------------------------
     # - Read and merge the geotiff files
     # ----------------------------------------------------------------
-    geoFiles <- list.files("~/tmp/TDM90",'.*(.tif)$')
     DEM <- NULL
-    for ( file in geoFiles ) {
-        tmp <- raster( sprintf("~/tmp/TDM90/%s", file) )
-        NAvalue(tmp) <- -32767
-        tmp <- aggregate(tmp, fact = fact)
-        if ( is.null(DEM) ) { DEM <- tmp; next }
-        DEM <- merge(DEM, tmp)
+    if(return.raster) {
+	geoFiles <- list.files("~/tmp/TDM90",'.*(.tif)$')
+	for ( file in geoFiles ) {
+	    tmp <- raster( sprintf("~/tmp/TDM90/%s", file) )
+	    NAvalue(tmp) <- -32767
+	    tmp <- aggregate(tmp, fact = fact)
+	    if ( is.null(DEM) ) { DEM <- tmp; next }
+	    DEM <- merge(DEM, tmp)
+	}
     }
- 
     # ----------------------------------------------------------------
     # Return Raster object
     # ----------------------------------------------------------------
